5e87f160841051f5e61b6aff211d7263968047b7
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-02-06 17:36:19 +0900

    useState : 시간 Count

--- src/components/UseState.jsx
+++ src/components/UseState.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 export const UseState = () => {
+   const [time, setTime] = useState(1); // 시간 기본 업데이트
+   const handleClick = () => {
+      let newTime; // 시간 저장 변수
+      if(time >= 24){  // 24시가 넘을경우 0시로 변경
+         newTime = 0;
+      }else { // 24시가 안넘을 경우 1시간씩 추가
+         newTime = time + 1;
+      }
+      setTime(newTime); // 시간 업데이트
+   }
     return (
         <>
-            <div className="">UseState</div>
+         <div className="">
+            <span>현재 시각:{time}</span>
+            <button onClick={handleClick}>Update</button>     
+         </div>
         </>
     );
 }
